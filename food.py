b3b34de7163baf64eb90730b848fd78bff3b4ada
JIVESH HAMAL <bravojivesh@gmail.com>
2024-02-04 15:45:09 +0530

    Everything works, except out of bound and Score

--- food.py
+++ food.py
@@ -6,7 +6,7 @@ y_cor = random.randrange(-250, 250)
 
 class Food:
 
-    def __init__(self):
+    def __init__(self): #previously we created a sub-class of Turtle
         food=Turtle()
         food.shape("circle")
         food.penup()
@@ -15,7 +15,8 @@ class Food:
         self.ycor=y_cor
         # self.reset_food()
         food.goto(x_cor, y_cor)
-        self.foodx=food
+        self.foodx=food #this was not needed previously, as it was a sub-class. We could directly call the turtle attributes throughout
+        # the class. 
 
     def reset(self):
         x_cor1 = random.randrange(-250, 250)
